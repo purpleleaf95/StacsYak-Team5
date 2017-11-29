13c2c47c5fefa487536de1f737f320d0c3911efb
Chen <Carol.PanChen@outlook.com>
2017-11-29 11:12:59 +0000

    Create displayDelete() function

---
+++ CS5002-A4-StacsYak-displayDelete.js
@@ -0,0 +1,8 @@
+//  This function aims to delete a Yak
+function displayDelete() {
+  //  Set style
+  document.getElementById("showGet").style.display = "none";
+  document.getElementById("showPost").style.display = "none";
+  document.getElementById("showVote").style.display = "none";
+  document.getElementById("showDelete").style.display = "block";
+}
