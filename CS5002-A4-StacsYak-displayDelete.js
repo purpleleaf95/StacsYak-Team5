68ff40124f2e91414461507a94f9ccee1906eaf2
mufton <mtufton1994@8afbf8e5.st-andrews.ac.uk>
2017-11-29 18:00:42 +0000

    working

--- CS5002-A4-StacsYak-displayDelete.js
+++ CS5002-A4-StacsYak-displayDelete.js
@@ -5,4 +5,18 @@ function displayDelete() {
   document.getElementById("showPost").style.display = "none";
   document.getElementById("showVote").style.display = "none";
   document.getElementById("showDelete").style.display = "block";
+
+}
+
+
+function del() {
+  var postID = document.getElementById("delete").value;
+
+  fetch(URL + postID + keyQuery, {
+    method: "DELETE"
+  }).then (response => response.json())
+    .then (yaks => { console.log("the yak id is" + postID);
+
+    })
+    .catch(error => console.log(error));
 }
