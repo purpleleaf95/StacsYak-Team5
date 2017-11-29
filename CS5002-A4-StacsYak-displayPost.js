dbbdee374b53c2c093884489bb0b811a4b59c143
Allison <jing.allison@gmail.com>
2017-11-29 13:09:43 +0000

    PostRevised

--- CS5002-A4-StacsYak-displayPost.js
+++ CS5002-A4-StacsYak-displayPost.js
@@ -6,3 +6,20 @@ function displayPost() {
   document.getElementById("showVote").style.display = "none";
   document.getElementById("showDelete").style.display = "none";
 }
+
+function postYak(){
+  var message = {
+    content: document.getElementById("postContent")
+  };
+  const msg = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(message)
+  };
+
+//  Send HTTP request
+fetch(URL,msg)
+.then(response => response.json());
+}
