f41802290f5eb7353f3d983f48d2a2eb2415eaad
Chen <Carol.PanChen@outlook.com>
2017-11-29 11:13:25 +0000

    Create displayPost() function

---
+++ CS5002-A4-StacsYak-displayPost.js
@@ -0,0 +1,8 @@
+//  This function aims to post a Yak
+function displayPost() {
+  //  Set style
+  document.getElementById("showGet").style.display = "none";
+  document.getElementById("showPost").style.display = "block";
+  document.getElementById("showVote").style.display = "none";
+  document.getElementById("showDelete").style.display = "none";
+}
