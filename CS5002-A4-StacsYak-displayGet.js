1a4b7c97c18906e04851fa8f77e03dab5641ed2d
Chen <Carol.PanChen@outlook.com>
2017-11-29 22:32:44 +0000

    Add error

--- CS5002-A4-StacsYak-displayGet.js
+++ CS5002-A4-StacsYak-displayGet.js
@@ -11,35 +11,38 @@ function displayGet(){
   fetch(URL + keyQuery)
     .then(response => response.json())
     .then(data => {
-      table.innerHTML = "";
-      //This function aims to add a cell to a row
-      var addCell = function(tag, text) {
-          var tag = document.createElement(String(tag));
-          var txt = document.createTextNode(text);
-          tag.appendChild(txt);
-          tr.appendChild(tag);
-      };
-      //  Show the table's head
-      let tr = document.createElement("tr");
-      addCell("th", "TIME");
-      addCell("th", "ID");
-      addCell("th", "USER NAME");
-      addCell("th", "CONTENT");
-      addCell("th", "TOTAL VOTES");
-      addCell("th", "USER VOTE");
-      table.appendChild(tr);
-      //  Show the table's contents
-      for(let index = 0; index < data.length; index++){
-        //  Create a new row
-        tr = document.createElement("tr");
-        addCell("td", data[index]["timestamp"]);
-        addCell("td", data[index]["id"]);
-        addCell("td", data[index]["userNick"]);
-        addCell("td", data[index]["content"]);
-        addCell("td", data[index]["votes"]);
-        addCell("td", data[index]["userVote"]);
+      if(data["error"] != undefined){
+        table.innerHTML = String(data["error"]);
+      } else {
+        table.innerHTML = "";
+        //This function aims to add a cell to a row
+        var addCell = function(tag, text) {
+            var tag = document.createElement(String(tag));
+            var txt = document.createTextNode(text);
+            tag.appendChild(txt);
+            tr.appendChild(tag);
+        };
+        //  Show the table's head
+        let tr = document.createElement("tr");
+        addCell("th", "TIME");
+        addCell("th", "ID");
+        addCell("th", "USER NAME");
+        addCell("th", "CONTENT");
+        addCell("th", "TOTAL VOTES");
+        addCell("th", "USER VOTE");
         table.appendChild(tr);
+        //  Show the table's contents
+        for(let index = 0; index < data.length; index++){
+          //  Create a new row
+          tr = document.createElement("tr");
+          addCell("td", data[index]["timestamp"]);
+          addCell("td", data[index]["id"]);
+          addCell("td", data[index]["userNick"]);
+          addCell("td", data[index]["content"]);
+          addCell("td", data[index]["votes"]);
+          addCell("td", data[index]["userVote"]);
+          table.appendChild(tr);
+        }
       }
-    })
-    .catch(error => table.innerHTML = error);
+    });
 }
