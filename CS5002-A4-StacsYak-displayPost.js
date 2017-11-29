73848624b8fc032a0ccdb97a609ddebe555a4689
Allison <jing.allison@gmail.com>
2017-11-29 20:48:24 +0000

    Post finished

--- CS5002-A4-StacsYak-displayPost.js
+++ CS5002-A4-StacsYak-displayPost.js
@@ -1,3 +1,9 @@
+//  Set key and URL
+// var key = "a4d043e0-87cb-4854-9a01-5304747c83b5";
+// var URL = "https://cs5002-api.host.cs.st-andrews.ac.uk/api/yaks?key=" + key;
+
+var urlLocal = "https://cs5002-api.host.cs.st-andrews.ac.uk/api/yaks?key=a4d043e0-87cb-4854-9a01-5304747c83b5"; //works
+//var urlUser = "https://cs5002-api.host.cs.st-andrews.ac.uk/api/user?key=a4d043e0-87cb-4854-9a01-5304747c83b5";  //does not work yet
 //  This function aims to post a Yak
 function displayPost() {
   //  Set style
@@ -5,11 +11,13 @@ function displayPost() {
   document.getElementById("showPost").style.display = "block";
   document.getElementById("showVote").style.display = "none";
   document.getElementById("showDelete").style.display = "none";
+  //document.getElementById("showSucc").style.display="none";
 }
+
 
 function postYak(){
   var message = {
-    content: document.getElementById("postContent")
+    "content": document.getElementById("postContent").value
   };
   const msg = {
     method: "POST",
@@ -20,6 +28,9 @@ function postYak(){
   };
 
 //  Send HTTP request
-fetch(URL,msg)
-.then(response => response.json());
+fetch(urlLocal,msg)
+.then(response => response.json())
+//.then(afterPost =>document.getElementById("showSucc").style.display="block")
+.catch(error=>document.getElementById("showError").innerHTML = error);
+document.getElementById("postContent").value = '';
 }
