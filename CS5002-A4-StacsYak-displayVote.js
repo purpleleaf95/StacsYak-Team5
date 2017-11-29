6cbb47e03c27ecf35281bc38915aad4cf1b0f69b
Chen <Carol.PanChen@outlook.com>
2017-11-29 11:13:34 +0000

    Create displayVote() function

---
+++ CS5002-A4-StacsYak-displayVote.js
@@ -0,0 +1,8 @@
+//  This function aims to vote a Yak
+function displayVote() {
+  //  Set style
+  document.getElementById("showGet").style.display = "none";
+  document.getElementById("showPost").style.display = "none";
+  document.getElementById("showVote").style.display = "block";
+  document.getElementById("showDelete").style.display = "none";
+}
