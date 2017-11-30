0085562b9791dbf227774c48cee59e2ec1c57ad9
Chen <Carol.PanChen@outlook.com>
2017-11-30 20:06:31 +0000

    Update

--- CS5002-A4-StacsYak-displayAllYak.js
+++ CS5002-A4-StacsYak-displayAllYak.js
@@ -5,10 +5,32 @@ function displayAllYak(){
   document.getElementById("showMyYak").style.display = "none";
   document.getElementById("showMyProfile").style.display = "none";
   let table = document.getElementById("allYakResult");
+  //  This function aims to add an option to the end of a select list
+  function addOption(selectList, data) {
+      let option = document.createElement("option");
+      option.value = data;
+      option.text = String(data);
+      selectList.appendChild(option);
+  };
+  //  Add options of fixed select lists
+  let selectedMinHour = document.getElementById("selectByMinHour");
+  for (let hour = 2; hour <= 48; hour++) {
+    addOption(selectedMinHour, hour);
+  }
+  let selectedMaxHour = document.getElementById("selectByMaxHour");
+  for (let hour = 47; hour >= 1; hour --) {
+    addOption(selectedMaxHour, hour);
+  }
+  let selectedMyVoteType = document.getElementById("selectByMyVoteType");
+  addOption(selectedMyVoteType, "none");
+  addOption(selectedMyVoteType, "up");
+  addOption(selectedMyVoteType, "down");
   //  Send HTTP request
   fetch(URL + "/yaks" + keyQuery)
     .then(response => response.json())
     .then(data => {
+      //  Add options of dynamic select lists
+      
       if(data["error"] != undefined){
         table.innerHTML = String("Error: " + data["error"]);
       } else {
