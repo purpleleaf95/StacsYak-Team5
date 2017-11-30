0ab4ddfe7ea099903fe8dbfb1831a94aa1d9961b
Chen <Carol.PanChen@outlook.com>
2017-11-30 16:13:17 +0000

    Change error

--- CS5002-A4-StacsYak-displayAllYak.js
+++ CS5002-A4-StacsYak-displayAllYak.js
@@ -10,7 +10,7 @@ function displayAllYak(){
     .then(response => response.json())
     .then(data => {
       if(data["error"] != undefined){
-        table.innerHTML = String("Error: " + data["error"]);
+        document.getElementById("showAllYak").innerHTML = String("Error: " + data["error"]);
       } else {
         table.innerHTML = "";
         //This function aims to add a cell to a row
