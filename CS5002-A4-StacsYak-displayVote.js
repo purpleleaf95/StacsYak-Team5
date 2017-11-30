5807e87ec2378dd3f5d220e5e37ac7a9a44b469b
Chen <Carol.PanChen@outlook.com>
2017-11-30 00:29:57 +0000

    Put two functions together + Add alert and error

--- CS5002-A4-StacsYak-displayVote.js
+++ CS5002-A4-StacsYak-displayVote.js
@@ -1,4 +1,4 @@
-//  This function aims to vote a Yak
+//  This function aims to set the style of vote button
 function displayVote() {
   //  Set style
   document.getElementById("showGet").style.display = "none";
@@ -8,46 +8,37 @@ function displayVote() {
   document.getElementById("showUser").style.display = "none";
 }
 
-function upVote(){
-  var yakId = document.getElementById("voteID").value;
-  var vote = document.getElementById("up").value;
-
-var message = {
-  direction: vote
-};
-
-const msg = {
-  method: "POST",
-  headers: {
-    "Content-Type":"application/json"
-  },
-  body: JSON.stringify(message)
-};
-
-//  Send HTTP request
-fetch(URL + yakId + "/vote" + keyQuery, msg)
-.then(response => response.json())
-.catch(error => console.log(error));
+//  This function aims to up/down vote a Yak
+function vote(voteType){
+  const yakId = document.getElementById("voteID").value;
+  const message = {
+    direction: voteType
+  };
+  const initObject = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(message)
+  };
+  //  Send HTTP request
+  fetch(URL + "/" + yakId + "/vote" + keyQuery, initObject)
+  .then(response => response.json())
+  .then(data => {
+    if(data["error"] == undefined) {
+      alert("Yak is successfully voted.");
+    } else {
+      alert("Vote failed. (Error: " + data["error"] + ")");
+    }
+  })
 }
 
+//  This function aims to up vote a Yak
+function upVote(){
+  vote(document.getElementById("up").value)
+}
+
+//  This function aims to down vote a Yak
 function downVote(){
-  var yakId = document.getElementById("voteID").value;
-  var vote = document.getElementById("down").value;
-
-var message = {
-  direction: vote
-};
-
-const msg = {
-  method: "POST",
-  headers: {
-    "Content-Type":"application/json"
-  },
-  body: JSON.stringify(message)
-};
-
-//  Send HTTP request
-fetch(URL + yakId + "/vote" + keyQuery, msg)
-.then(response => response.json())
-.catch(error => console.log(error));
+  vote(document.getElementById("down").value)
 }
