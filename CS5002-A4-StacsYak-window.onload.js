40cc9e69cc88880761e20479d17d2c2ac3ddf6d4
Chen <Carol.PanChen@outlook.com>
2017-11-30 14:10:46 +0000

    window.onload

---
+++ CS5002-A4-StacsYak-window.onload.js
@@ -0,0 +1,8 @@
+//  Set key and URL
+const key = "a4d043e0-87cb-4854-9a01-5304747c83b5";
+const keyQuery = "?key=" + key;
+const URL = "https://cs5002-api.host.cs.st-andrews.ac.uk/api";
+
+window.onload = function() {
+  displayAllYak();
+}
