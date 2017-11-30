baa6a8599a1e1f949e5a344d2b25799ef2d993d5
mufton <mtufton1994@8afbf8e5.st-andrews.ac.uk>
2017-11-30 02:06:20 +0000

    working buttons

--- CS5002-A4-StacsYak-displayVote.js
+++ CS5002-A4-StacsYak-displayVote.js
@@ -8,58 +8,58 @@ function displayVote() {
   document.getElementById("showUser").style.display = "none";
 }
 
-<<<<<<< HEAD
-function upVote(yakId){
-  //var yakId = document.getElementById("voteID").value;
-  //var vote = document.getElementById("up").value;
-  //var yakId = document.getElementById("up").value;
-  vote = "up";
-var message = {
-  direction: vote
-};
-
-const msg = {
-  method: "POST",
-  headers: {
-    "Content-Type":"application/json"
-  },
-  body: JSON.stringify(message)
-};
-
-//  Send HTTP request
-fetch(URL+ "/" + yakId + "/vote" + keyQuery, msg)
-.then(response => response.json())
-.catch(error => console.log(error));
-}
-
-function downVote(yakId){
-//  var yakId = document.getElementById("voteID").value;
-//  var vote = document.getElementById("down").value;
-
-//only picking up first element with down element
-//var yakId = document.getElementById("down").value;
-vote = "down";
-
-var message = {
-  direction: vote
-};
-
-const msg = {
-  method: "POST",
-  headers: {
-    "Content-Type":"application/json"
-  },
-  body: JSON.stringify(message)
-};
-
-//  Send HTTP request
-fetch(URL +"/"+ yakId + "/vote" + keyQuery, msg)
-.then(response => response.json())
-.catch(error => console.log(error));
-=======
+// <<<<<<< HEAD
+// function upVote(yakId){
+//   //var yakId = document.getElementById("voteID").value;
+//   //var vote = document.getElementById("up").value;
+//   //var yakId = document.getElementById("up").value;
+//   vote = "up";
+// var message = {
+//   direction: vote
+// };
+//
+// const msg = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type":"application/json"
+//   },
+//   body: JSON.stringify(message)
+// };
+//
+// //  Send HTTP request
+// fetch(URL+ "/" + yakId + "/vote" + keyQuery, msg)
+// .then(response => response.json())
+// .catch(error => console.log(error));
+// }
+//
+// function downVote(yakId){
+// //  var yakId = document.getElementById("voteID").value;
+// //  var vote = document.getElementById("down").value;
+//
+// //only picking up first element with down element
+// //var yakId = document.getElementById("down").value;
+// vote = "down";
+//
+// var message = {
+//   direction: vote
+// };
+//
+// const msg = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type":"application/json"
+//   },
+//   body: JSON.stringify(message)
+// };
+//
+// //  Send HTTP request
+// fetch(URL +"/"+ yakId + "/vote" + keyQuery, msg)
+// .then(response => response.json())
+// .catch(error => console.log(error));
+// =======
 //  This function aims to up/down vote a Yak
-function vote(voteType){
-  const yakId = document.getElementById("voteID").value;
+function vote(voteType, yakId){
+  //const yakId = document.getElementById("voteID").value;
   const message = {
     direction: voteType
   };
@@ -83,12 +83,12 @@ function vote(voteType){
 }
 
 //  This function aims to up vote a Yak
-function upVote(){
-  vote(document.getElementById("up").value)
+function upVote(yakId){
+  vote("up", yakId)
 }
 
 //  This function aims to down vote a Yak
-function downVote(){
-  vote(document.getElementById("down").value)
->>>>>>> origin/master
+function downVote(yakId){
+  vote("down", yakId)
+// >>>>>>> origin/master
 }
